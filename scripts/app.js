06107f86a0dfa71228089b716b19841874d4ff97
yosse95ai <karateyosi@gmail.com>
2020-04-28 16:09:08 +0900

    県内市町村リストを表示できる機能を追加

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 'use strict';
 const weather = require('../lib/weatherFunc');
+const locate = require('../lib/setLocation');
 
 module.exports = robot => {
   //指定地周辺の現在の天気を表示
@@ -37,12 +38,29 @@ module.exports = robot => {
       msg.send(message);
     })
   });
+  //県に存在する市町村を表示
+  robot.respond(/list (.+)/i, msg => {
+    const prefecture = msg.match[1].trim();
+    Promise.all([
+      locate.cityList(prefecture)
+      ]).then(cities => {
+        let t = "";
+        let prefectureNum = cities[0].length;
+        for(let k = 0; k < prefectureNum; k++){
+          t += '`'+cities[0][k].city+'` ';
+        }
+        msg.send(prefecture+'の市町村リスト('+prefectureNum+')\n'+t);
+      }).catch(message => {
+        msg.send(message);
+      });
+  });
   //ヘルプの表示
   robot.respond(/ヘルプ|help/i, msg => {
     const commands = {
-      '@wetter weather c {市町村名}':'市町村周辺の現在の天気を出力',
-      '@wetter weather o {郵便番号} {該当国ID}':'該当郵便番号の天気',
-      '@wetter weather p {日本郵便番号}':'該当郵便番号の天気',
+      '@wetter weather c {市町村名}':'市町村周辺の現在の天気を表示',
+      '@wetter weather o {郵便番号} {該当国ID}':'該当郵便番号の天気を表示',
+      '@wetter weather p {日本郵便番号}':'該当郵便番号の天気を表示',
+      '@wetter list':'指定県に存在する市町村を表示',
       '@wetter 例|ex':'例文を表示',
       '@wetter ヘルプ|help':'ヘルプを表示'
     }
@@ -57,7 +75,8 @@ module.exports = robot => {
   robot.respond(/例|ex/i, msg => {
     const ex = ['@wetter weather c 千代田区',
                 '@wetter weather o 94040 us',
-                '@wetter weather p 100-0001'
+                '@wetter weather p 100-0001',
+                '@wetter list 東京都'
     ];
     for (let i in ex) {
       msg.send(ex[i]);
